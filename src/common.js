893e7b54299b2cb25c34b77cea14c3641f35daaf
jdlrobson <jdlrobson@gmail.com>
2020-06-16 16:54:31 -0700

    cleanup

--- src/common.js
+++ src/common.js
@@ -13,7 +13,7 @@ Morbi sed maximus ex, in tincidunt nibh. Praesent tempus nibh ac dolor pretium e
 </div>`
 
 const placeholder = ( msg, height ) => {
-    return `<div style="width: 100%; height: ${height || 200}px; margin-bottom: 2px;
+    return `<div style="width: 100%; color: black; height: ${height || 200}px; margin-bottom: 2px;
             font-size: 12px; padding: 8px; box-sizing: border-box;
             display: flex; background: #eee; align-items: center;justify-content: center;">${msg}</div>`;
 };
